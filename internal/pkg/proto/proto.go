684864fed313a825009815061c147e96353a69ff
dzwdz <kg67199@gmail.com>
2022-06-21 19:17:08 +0200

    proto: split commands into verbs and nouns

--- internal/pkg/proto/proto.go
+++ internal/pkg/proto/proto.go
@@ -1,16 +1,19 @@
 package proto
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"ospm/internal/pkg/otp"
 	"ospm/internal/pkg/storage"
+	"strings"
 )
 
 var Debug = false
 
-func requireTOTP(c net.Conn, totp otp.Ratelimited) bool {
+func requireTOTP(c net.Conn, reader io.Reader, totp otp.Ratelimited) bool {
 	fmt.Fprintf(c, "NEED TOTP\n")
 	if Debug {
 		for i := -1; i <= 1; i++ {
@@ -19,7 +22,7 @@ func requireTOTP(c net.Conn, totp otp.Ratelimited) bool {
 	}
 
 	var pass int
-	_, err := fmt.Fscanf(c, "TOTP %d\n", &pass)
+	_, err := fmt.Fscanf(reader, "TOTP %d\n", &pass)
 	if err != nil {
 		fmt.Fprintf(c, "IERROR expected TOTP\n")
 		return false
@@ -39,18 +42,24 @@ func HandleClient(c net.Conn, totp otp.Ratelimited, db storage.Storage) {
 		fmt.Fprintf(c, "DEBUG i am insecure\n")
 	}
 
-	var cmd string
-	if _, err := fmt.Fscanf(c, "%s\n", &cmd); err != nil {
+	reader := bufio.NewReader(c)
+	cmd, err := reader.ReadString('\n')
+	if err != nil {
 		fmt.Fprintf(c, "IERROR ?\n")
 		return
 	}
+	cmd = strings.TrimSpace(cmd)
+	verb, noun, _ := strings.Cut(cmd, " ")
+
 	switch {
-	case cmd == "LIST":
+	case verb == "LIST":
 		for _, file := range db.List() {
 			fmt.Fprintf(c, "LIST %s\n", file)
 		}
-	case Debug && cmd == "TOTP_TEST":
-		if !requireTOTP(c, totp) {
+	case verb == "PING":
+		fmt.Fprintf(c, "PONG %s\n", noun)
+	case Debug && verb == "TOTP_TEST":
+		if !requireTOTP(c, reader, totp) {
 			return
 		}
 		fmt.Fprintf(c, "DEBUG passed auth\n")
